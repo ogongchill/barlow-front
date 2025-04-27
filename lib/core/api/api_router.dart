9f82cd25d39e521985750ec81530a10058750725
Sy P <yongsa0221@gmail.com>
2025-04-28 02:54:08 +0900

    feature: recent bill router 추가

--- lib/core/api/api_router.dart
+++ lib/core/api/api_router.dart
@@ -1,5 +1,8 @@
 import 'package:front/core/api/common/api_client.dart';
 import 'package:front/core/api/legislation-account/legislation_account_router.dart';
+import 'package:front/core/api/menu/menu_router.dart';
+import 'package:front/core/api/pre-announce/pre_announce_router.dart';
+import 'package:front/core/api/recent-bill/recent_bill_router.dart';
 
 import 'auth/auth_router.dart';
 import 'home/home_router.dart';
@@ -9,9 +12,15 @@ class ApiRouter {
   final AuthRouter authRouter;
   final HomeRouter homeRouter;
   final LegislationAccountRouter legislationAccountRouter;
+  final MenuRouter menuRouter;
+  final PreAnnounceRouter preAnnounceRouter;
+  final RecentBillRouter recentBillRouter;
 
   ApiRouter({required ApiClient apiClient})
     : authRouter = AuthRouter(apiClient),
       homeRouter = HomeRouter(apiClient),
-      legislationAccountRouter = LegislationAccountRouter(apiClient);
+      legislationAccountRouter = LegislationAccountRouter(apiClient),
+      preAnnounceRouter = PreAnnounceRouter(apiClient),
+      menuRouter = MenuRouter(apiClient),
+      recentBillRouter = RecentBillRouter(apiClient);
 }
