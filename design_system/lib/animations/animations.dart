df3c20ea8d91d7cf6ba6fb8e492905da9ee2228f
Sy P <yongsa0221@gmail.com>
2025-06-18 03:05:34 +0900

    chore: 누락된 파일 추가

---
+++ design_system/lib/animations/animations.dart
@@ -0,0 +1,6 @@
+import 'package:design_system/package_path.dart';
+
+class AnimationAssets {
+
+  static const String fireWorkPath = '$packagePath/assets/animations/lottie/simple_fireworks.json';
+}
