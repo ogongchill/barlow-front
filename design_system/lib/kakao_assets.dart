516e5adf6d1804390b60876e393537de786f2fa1
Sy P <yongsa0221@gmail.com>
2026-02-25 09:57:10 +0900

    feat: kakao관련 에셋 추가

---
+++ design_system/lib/kakao_assets.dart
@@ -0,0 +1,9 @@
+import 'package:design_system/package_path.dart';
+
+class KakaoAssets {
+
+  static const loginLargeNarrow = '$packagePath/assets/kakao/kakao_login_large_narrow.png';
+  static const loginLargeWide = '$packagePath/assets/kakao/kakao_login_large_wide.png';
+  static const loginMediumNarrow = '$packagePath/assets/kakao/kakao_login_medium_narrow.png';
+  static const loginMediumWide = '$packagePath/assets/kakao/kakao_login_medium_wide.png';
+}
